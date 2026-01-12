c689edac08d1c1f07dda9a0f143d274cb2f1c26c
Zuzana Osobová <zuzana.osobova@gmail.com>
2026-01-12 15:28:02 +0100

    Načtení dat z databáze a načtení do listu společně s detail toggle

--- frontend-vanilla/src/seznam.ts
+++ frontend-vanilla/src/seznam.ts
@@ -5,9 +5,40 @@ const app = document.getElementById('app');
 
 if (app){
 
+    //Načtení kontaktů z API
+    const response = await fetch('/api/contacts');
+    const data = await response.json();
+    const contacts = data.data;
+    console.log('contacts', contacts);
 
 
-// Attach one delegated listener to the list
+
+    contacts.forEach(contact => {
+        const li = document.createElement('li');
+        li.setAttribute('class', 'contact');
+
+
+
+        li.setAttribute('class', 'contact');
+
+        li.innerHTML = `<div class="contact-name">${contact.firstName} ${contact.lastName}</div>
+                            <div class="detail-panel">
+                                ${contact.email}
+                            <button class="delete-btn">Delete</button>
+                          </div>
+                        `;
+
+        li.addEventListener('click', () => {
+            // Zobrazit detail
+        });
+
+        const list = document.getElementById('contacts-list');
+        if (list) {list.appendChild(li);}
+    });
+
+
+
+    // List Listener for display toggle
     const contactsList = document.getElementById('contacts-list');
 
     if (contactsList){
@@ -16,16 +47,14 @@ if (app){
             const target = e.target;
             if (!(target instanceof Element)) return;
 
-            console.log(target);
-
-            // 1) Toggle detail when clicking the contact name
             if (target.classList.contains('contact-name')) {
                 const contactItem = target.closest('.contact');
                 const detail = contactItem?.querySelector('.detail-panel') as HTMLElement;
 
                 if (!detail) return;
 
-                if (detail.style.display === 'none'){ detail.style.display = 'block';
+                if (detail.style.display === 'none') {
+                    detail.style.display = 'block';
                 } else {
                     detail.style.display = 'none'; }
             }
