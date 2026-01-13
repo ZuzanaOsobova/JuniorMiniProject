647e6e4987432f6aab156299a7c8ae125d1efc3f
Zuzana Osobová <zuzana.osobova@gmail.com>
2026-01-13 09:36:21 +0100

    Upraven kód pro display toggle. Vytvořen výpis dat v detailech. Data očištěna od undefined

--- frontend-vanilla/src/seznam.ts
+++ frontend-vanilla/src/seznam.ts
@@ -17,33 +17,48 @@ if (app){
         const li = document.createElement('li');
         li.setAttribute('class', 'contact');
 
+        //data očištěná od undefined
+        const firstName = contact.firstName ? contact.firstName : '';
+        const lastName = contact.lastName ? contact.lastName : '';
+        const email = contact.email ? contact.email : '';
+        const phone = contact.phone ? contact.phone : '';
+        const note = contact.note ? contact.note : '';
+        const gender = contact.gender ? contact.gender : 'Preferred not to say';
+        const birthday = contact.birthday ? contact.birthday : '';
+
+        const city = contact.city ? contact.city : '';
+        const street = contact.streeting ? contact.streeting : '';
+        const houseNumber = contact.houseNumber ? contact.houseNumber : '';
+        const zipCode = contact.zipCode ? contact.zipCode : '';
+
+
 
 
         li.setAttribute('class', 'contact');
 
-        li.innerHTML = `<div class="contact-name">${contact.firstName} ${contact.lastName}</div>
+        li.innerHTML = `<div class="contact-name">${firstName} ${lastName}</div>
                             <div class="detail-panel">
-                                ${contact.email}
+                            Email: ${email} <br>
+                            Phone: ${phone} <br>
+                            Note: ${note} <br>
+                            Gender: ${gender} <br>
+                            Birthday: ${birthday} <br>
+                            
+                            <fieldset>
+                                <legend>Adress</legend>
+                                City: ${city} <br>
+                                Street: ${street} <br>
+                                House Number: ${houseNumber} <br>
+                                Zip Code: ${zipCode} <br>
+                            </fieldset> 
+                            
+                            <br>
+                            
                             <button class="delete-btn">Delete</button>
                           </div>
                         `;
 
-        li.addEventListener('click', () => {
-            // Zobrazit detail
-        });
-
-        const list = document.getElementById('contacts-list');
-        if (list) {list.appendChild(li);}
-    });
-
-
-
-    // List Listener for display toggle
-    const contactsList = document.getElementById('contacts-list');
-
-    if (contactsList){
-
-        contactsList.addEventListener('click', (e: MouseEvent) => {
+        li.addEventListener('click', (e) => {
             const target = e.target;
             if (!(target instanceof Element)) return;
 
@@ -58,8 +73,11 @@ if (app){
                 } else {
                     detail.style.display = 'none'; }
             }
-
         });
-    }
+
+        const list = document.getElementById('contacts-list');
+        if (list) {list.appendChild(li);}
+    });
+
 
 }
